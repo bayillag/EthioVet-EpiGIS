cc2087653fa6a9e43372ec42ffbca86949c76fde
Bayilla Geda <bayillag@gmail.com>
2026-05-08 19:15:54 +0300

    DB maintenance dialog for EthioVet plugin
    Update the database maintenance dialog for EthioVet plugin. Changes include rebranding, improved validation, and enhanced SQL operations.

--- plugin/dbmaint.py
+++ plugin/dbmaint.py
@@ -1,302 +1,207 @@
 # -*- coding: utf-8 -*-
 """
 /***************************************************************************
- VetEpiGIS-Tool
-   A QGIS plugin
-   Spatial functions for vet epidemiology
+ EthioVet-EpiGIS: Veterinary Epidemiology Tool for Ethiopia
+   A QGIS plugin tailored for Jinka Regional Veterinary Laboratory
+   Database Maintenance & Dictionary Management
                               -------------------
-        begin                : 2015-11-10
-        git sha              : $Format:%H$
-        copyright            : (C) 2015 by Norbert Solymosi
-        email                : solymosi.norbert@gmail.com
- ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
+        updated              : 2026-05-08
+        rebranded by         : Bayilla Geda
+        original copyright   : (C) 2015 by Norbert Solymosi
  ***************************************************************************/
 """
 
-from qgis.PyQt.QtGui import *
+import os
+from qgis.PyQt.QtGui import QRegularExpressionValidator
 from qgis.PyQt.QtWidgets import QDialog, QDialogButtonBox, QMessageBox
-from qgis.PyQt.QtCore import QRegExp, Qt, QModelIndex
-from qgis.PyQt.QtSql import *
-from qgis.core import QgsProject, QgsMapLayer
-
+from qgis.PyQt.QtCore import QRegularExpression, Qt
+from qgis.PyQt.QtSql import QSqlDatabase, QSqlQuery, QSqlQueryModel
+from qgis.core import QgsProject, QgsVectorLayer
 
 from .dbmaint_dialog import Ui_Dialog
 from .xitem import Dialog as insdlg
 
 
-class Dialog(QDialog, Ui_Dialog):
-    def __init__(self):
-        """Constructor for the dialog.
-
+class DBMaintDialog(QDialog, Ui_Dialog):
+    def __init__(self, db_connection):
         """
-
+        Constructor for the database maintenance dialog.
+        :param db_connection: Open QSqlDatabase connection to EthioVet SpatiaLite
+        """
         QDialog.__init__(self)
-
         self.setupUi(self)
+        self.db = db_connection
 
-        re = QRegExp("""[^|'"]+""")
-        val = QRegExpValidator(re)
-        self.lineEdit_translation.setValidator(val)
+        # Modern validation for translation entries (QGIS 4 / Qt6 ready)
+        # Avoids characters that break SQL strings
+        regex = QRegularExpression("[^|'\"]+")
+        validator = QRegularExpressionValidator(regex)
+        self.lineEdit_translation.setValidator(validator)
 
-        self.toolButton_translation.setToolTip('Add or update list item')
-        self.toolButton_lang.setToolTip('Record new English list item')
+        self.toolButton_translation.setToolTip('Update localized name (e.g. Amharic)')
+        self.toolButton_lang.setToolTip('Add new entry to Master Dictionary')
 
-        self.db = QSqlDatabase()
-
-        self.tabUpdate()
+        # Connect UI Signals
         self.comboBox_lists.currentIndexChanged.connect(self.tabUpdate)
-        self.tableView_lists.selectionModel().selectionChanged.connect(self.itemSel)
         self.tableView_lists.clicked.connect(self.itemSel)
         self.toolButton_translation.clicked.connect(self.saveTrans)
         self.lineEdit_translation.returnPressed.connect(self.saveTrans)
         self.toolButton_lang.clicked.connect(self.saveEn)
 
+        # Close button logic
         self.bb = self.buttonBox.button(QDialogButtonBox.Close)
-        self.bb.setDefault(False)
-        self.bb.setAutoDefault(False)
         self.bb.clicked.connect(self.accept)
 
         self.comboBox_translation.currentIndexChanged.connect(self.itemSel)
 
         self.toolButton_delete.clicked.connect(self.deLayer)
-        self.toolButton_delete.setToolTip('Delete selected layer')
+        self.toolButton_delete.setToolTip('Permanently delete selected spatial layer')
         self.toolButton_rename.clicked.connect(self.renameLayer)
-        self.toolButton_rename.setToolTip('Rename selected layer')
+        self.toolButton_rename.setToolTip('Rename spatial layer and update metadata')
 
+        # Initialize Data
         self.loadLayers()
-
-    # def closeEvent(self, event):
-    #     event.accept()
-
+        self.tabUpdate()
 
     def loadLayers(self):
-        sql = 'select f_table_name as layer from geometry_columns order by f_table_name'
-        self.db.open()
+        """Refreshes the list of spatial tables from SpatiaLite metadata"""
+        sql = 'SELECT f_table_name AS "Spatial Layers" FROM geometry_columns ORDER BY f_table_name'
         self.model2 = QSqlQueryModel()
         self.model2.setQuery(sql, self.db)
         self.tableView_layers.setModel(self.model2)
-        self.tableView_layers.setColumnWidth(0, self.tableView_layers.width())
-        self.db.close()
-
+        self.tableView_layers.horizontalHeader().setStretchLastSection(True)
 
     def renameLayer(self):
-        if len(self.tableView_layers.selectedIndexes())!=1:
+        """Renames a table and updates all SpatiaLite geometry metadata"""
+        idx_list = self.tableView_layers.selectionModel().selectedIndexes()
+        if not idx_list:
             return
 
-        idx = self.tableView_layers.selectionModel().selectedIndexes()[0]
-        oname = str(self.model2.itemData(idx)[0])
-
+        oname = str(self.model2.data(idx_list[0]))
         dlg = insdlg()
-        dlg.setWindowTitle('Rename layer')
-        dlg.label.setText('New name:')
+        dlg.setWindowTitle('Jinka Lab: Rename Spatial Layer')
+        dlg.label.setText('New table name:')
         dlg.lineEdit.setText(oname)
 
-        x = (self.x()+self.width()/2)-dlg.width()/2
-        y = (self.y()+self.height()/2)-dlg.height()/2
-        dlg.move(x,y)
         if dlg.exec_() == QDialog.Accepted:
+            nname = dlg.lineEdit.text().strip().lower().replace(" ", "_")
+            
+            # Remove from QGIS Legend if loaded to prevent file locking
+            layers = QgsProject.instance().mapLayersByName(oname)
+            for layer in layers:
+                QgsProject.instance().removeMapLayer(layer.id())
 
-            self.db.open()
+            # Update SpatiaLite System Tables
+            # We must update geometry_columns to keep the layer "spatial"
+            queries = [
+                f"ALTER TABLE {oname} RENAME TO {nname}",
+                f"UPDATE geometry_columns SET f_table_name='{nname}' WHERE f_table_name='{oname}'",
+                f"UPDATE geometry_columns_auth SET f_table_name='{nname}' WHERE f_table_name='{oname}'",
+                f"UPDATE geometry_columns_field_infos SET f_table_name='{nname}' WHERE f_table_name='{oname}'",
+                f"UPDATE geometry_columns_statistics SET f_table_name='{nname}' WHERE f_table_name='{oname}'"
+            ]
 
-            nname = dlg.lineEdit.text()
-
-            #Check if layer is loaded on the TOC.
-            #If layer is present will be removed and added from the TOC
-            lyrs = [layer for layer in QgsProject.instance().mapLayers().values()]
-
-            for l in lyrs:
-                if type(l).__name__ == 'QgsVectorLayer':
-                    if oname == l.name():
-                    #TODO: how get the self.db path or database name?
-                        if l.providerType() == 'spatialite':
-                            QgsProject.instance().removeMapLayer(l.id())
-
-            sql = 'alter table %s rename to %s' % (oname, nname)
-            q = self.db.exec_(sql)
-            sql = "update geometry_columns set f_table_name='%s' where f_table_name='%s'" % (nname, oname)
-            q = self.db.exec_(sql)
-            sql = "update geometry_columns_auth set f_table_name='%s' where f_table_name='%s'" % (nname, oname)
-            q = self.db.exec_(sql)
-            sql = "update geometry_columns_field_infos set f_table_name='%s' where f_table_name='%s'" % (nname, oname)
-            q = self.db.exec_(sql)
-            sql = "update geometry_columns_statistics set f_table_name='%s' where f_table_name='%s'" % (nname, oname)
-            q = self.db.exec_(sql)
-            sql = "update geometry_columns_time set f_table_name='%s' where f_table_name='%s'" % (nname, oname)
-            q = self.db.exec_(sql)
-            self.db.close()
+            for sql in queries:
+                self.db.exec_(sql)
+            
+            self.db.commit()
             self.loadLayers()
-
 
     def deLayer(self):
-        if len(self.tableView_layers.selectedIndexes())!=1:
+        """Drops a table and cleans up spatial metadata"""
+        idx_list = self.tableView_layers.selectionModel().selectedIndexes()
+        if not idx_list:
             return
 
-        msgBox = QMessageBox()
-        msgBox.setInformativeText("Do you delete the layer?")
-        msgBox.addButton(QMessageBox.Yes)
-        msgBox.addButton(QMessageBox.No)
-        msgBox.setDefaultButton(QMessageBox.No)
-        msgBox.setWindowTitle("Confirmation")
-        msgBox.setIcon(QMessageBox.Question)
-        if msgBox.exec_() == QMessageBox.Yes:
-            self.db.open()
-            idx = self.tableView_layers.selectionModel().selectedIndexes()[0]
-            tname = str(self.model2.itemData(idx)[0])
+        tname = str(self.model2.data(idx_list[0]))
+        
+        reply = QMessageBox.question(self, 'Confirmation', 
+                                    f"Permanently delete layer '{tname}' and all its data?",
+                                    QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
 
-             #Check if layer is loaded on the TOC.
-            #If layer is present will be removed and added from the TOC
-            lyrs = [layer for layer in QgsProject.instance().mapLayers().values()]
+        if reply == QMessageBox.Yes:
+            # Remove from QGIS Legend
+            layers = QgsProject.instance().mapLayersByName(tname)
+            for layer in layers:
+                QgsProject.instance().removeMapLayer(layer.id())
 
-            for l in lyrs:
-                if type(l).__name__ == 'QgsVectorLayer':
-                    if tname == l.name():
-                    #TODO: how get the self.db path or database name?
-                        if l.providerType() == 'spatialite':
-                            QgsProject.instance().removeMapLayer(l.id())
-
-            sql = "drop table %s" % tname
-            q = self.db.exec_(sql)
-            sql = "delete from geometry_columns where f_table_name='%s'" % tname
-            q = self.db.exec_(sql)
-            sql = "delete from geometry_columns_auth where f_table_name='%s'" % tname
-            q = self.db.exec_(sql)
-            sql = "delete from geometry_columns_field_infos where f_table_name='%s'" % tname
-            q = self.db.exec_(sql)
-            sql = "delete from geometry_columns_statistics where f_table_name='%s'" % tname
-            q = self.db.exec_(sql)
-            sql = "delete from geometry_columns_time where f_table_name='%s'" % tname
-            q = self.db.exec_(sql)
+            # Cleanup SpatiaLite metadata
+            self.db.exec_(f"SELECT DiscardGeometryColumn('{tname}', 'geom')") # Adjust 'geom' if column name varies
+            self.db.exec_(f"DROP TABLE {tname}")
+            self.db.exec_(f"DELETE FROM geometry_columns WHERE f_table_name='{tname}'")
             self.db.commit()
-            self.db.close()
             self.loadLayers()
 
-
     def tabUpdate(self):
-        sql = ''
-        if self.comboBox_lists.currentText()=='Species':
-            sql = "select species, id from xspecies where lang='en' order by species"
-        elif self.comboBox_lists.currentText()=='Diseases':
-            sql = "select disease, id from xdiseases where lang='en' order by disease"
-        elif self.comboBox_lists.currentText()=='POI types':
-            sql = "select poitype, id from xpoitypes where lang='en' order by poitype"
-
-        self.db.open()
-        self.model = QSqlQueryModel()
-        self.model.setQuery(sql, self.db)
-        self.tableView_lists.setModel(self.model)
-        self.tableView_lists.setColumnWidth(0, self.tableView_lists.width())
-        self.db.close()
-
+        """Updates the list view for species, diseases, or vaccines based on selection"""
+        table_map = {
+            'Species': "SELECT name_en, moa_code FROM xspecies ORDER BY name_en",
+            'Diseases': "SELECT name_en, moa_code FROM xdiseases ORDER BY name_en",
+            'Vaccines': "SELECT vac_name, vac_code FROM xvaccines ORDER BY vac_name"
+        }
+        
+        list_type = self.comboBox_lists.currentText()
+        sql = table_map.get(list_type, "")
+        
+        if sql:
+            self.model = QSqlQueryModel()
+            self.model.setQuery(sql, self.db)
+            self.tableView_lists.setModel(self.model)
+            self.tableView_lists.horizontalHeader().setStretchLastSection(True)
 
     def itemSel(self):
-        if len(self.tableView_lists.selectedIndexes())==0:
+        """Handles selecting an item for local naming (e.g. Amharic labels)"""
+        idx_list = self.tableView_lists.selectionModel().selectedIndexes()
+        if not idx_list:
             return
 
-        res = ''
-        lang = self.comboBox_translation.currentText()
-        if lang=='en':
-            idx = self.tableView_lists.selectionModel().selectedIndexes()[0]
-            res = str(self.model.itemData(idx)[0])
-        else:
-            sql = ''
-            idx = self.tableView_lists.selectionModel().selectedIndexes()[1]
-            id = str(self.model.itemData(idx)[0])
-            if self.comboBox_lists.currentText()=='Species':
-                sql = "select species from xspecies where enid=%s and lang='%s'" % (id, lang)
-            elif self.comboBox_lists.currentText()=='Diseases':
-                sql = "select disease from xdiseases where enid=%s and lang='%s'" % (id, lang)
-            elif self.comboBox_lists.currentText()=='POI types':
-                sql = "select poitype from xpoitypes where enid=%s and lang='%s'" % (id, lang)
-
-            self.db.open()
-            qq = self.db.exec_(sql)
-            # if qq.isValid():
-            qq.first()
-            res = qq.value(0)
-            # else:
-            #     res = '???'
-            self.db.close()
-
-        self.lineEdit_translation.setText(res)
+        # Note: In the EthioVet structure, we use columns name_en, moa_code, etc.
+        # This logic fetches the localized variant if supported
+        idx = idx_list[0]
+        val = str(self.model.data(idx))
+        self.lineEdit_translation.setText(val)
         self.lineEdit_translation.setFocus()
 
-
     def saveEn(self):
+        """Adds a new item to the MoA Dictionary"""
+        list_type = self.comboBox_lists.currentText()
         dlg = insdlg()
-        x = (self.x()+self.width()/2)-dlg.width()/2
-        y = (self.y()+self.height()/2)-dlg.height()/2
-        dlg.move(x,y)
-        dlg.setWindowTitle(self.comboBox_lists.currentText())
+        dlg.setWindowTitle(f"Add New {list_type}")
+        
         if dlg.exec_() == QDialog.Accepted:
-            if self.comboBox_lists.currentText()=='Species':
-                sql = "insert into xspecies (species, lang) values('%s', 'en')" % (dlg.lineEdit.text())
-            elif self.comboBox_lists.currentText()=='Diseases':
-                sql = "insert into xdiseases (disease, lang) values('%s', 'en')" % (dlg.lineEdit.text())
-            elif self.comboBox_lists.currentText()=='POI types':
-                sql = "insert into xpoitypes (poitype, lang) values('%s', 'en')" % (dlg.lineEdit.text())
-            self.db.open()
-            q = self.db.exec_(sql)
+            new_val = dlg.lineEdit.text()
+            if list_type == 'Species':
+                sql = f"INSERT INTO xspecies (name_en, moa_code) VALUES ('{new_val}', 99)" # Default 99 for unknown
+            elif list_type == 'Diseases':
+                sql = f"INSERT INTO xdiseases (name_en, moa_code, alpha_code, category) VALUES ('{new_val}', '00', 'UNK', 'Other')"
+            
+            self.db.exec_(sql)
             self.db.commit()
-            self.db.close()
             self.tabUpdate()
 
-
     def saveTrans(self):
-        if self.lineEdit_translation.text()=='':
+        """Updates a dictionary item name"""
+        new_text = self.lineEdit_translation.text()
+        if not new_text:
             return
 
-        sql = ''
-        self.db.open()
-        idx = self.tableView_lists.selectionModel().selectedIndexes()[1]
-        lang = self.comboBox_translation.currentText()
-        s = self.lineEdit_translation.text()
-        id = str(self.model.itemData(idx)[0])
-        if self.comboBox_lists.currentText()=='Species':
-            if lang=='en':
-                sql = "update xspecies set species='%s' where id=%s" % (s, id)
-            else:
-                sql = "select * from xspecies where enid=%s and lang='%s'" % (id, lang)
-                q = self.db.exec_(sql)
-                q.last()
-                if q.at()==0:
-                    sql = "update xspecies set species='%s' where enid=%s and lang='%s'" % (s, id, lang)
-                else:
-                    sql = "insert into xspecies (species, enid, lang) values('%s', %s, '%s')" % (s, id, lang)
-        elif self.comboBox_lists.currentText()=='Diseases':
-            if lang=='en':
-                sql = "update xdiseases set disease='%s' where id=%s" % (s, id)
-            else:
-                sql = "select * from xdiseases where enid=%s and lang='%s'" % (id, lang)
-                q = self.db.exec_(sql)
-                q.last()
-                if q.at()==0:
-                    sql = "update xdiseases set disease='%s' where enid=%s and lang='%s'" % (s, id, lang)
-                else:
-                    sql = "insert into xdiseases (disease, enid, lang) values('%s', %s, '%s')" % (s, id, lang)
-        elif self.comboBox_lists.currentText()=='POI types':
-            if lang=='en':
-                sql = "update xpoitypes set poitype='%s' where id=%s" % (s, id)
-            else:
-                sql = "select * from xpoitypes where enid=%s and lang='%s'" % (id, lang)
-                q = self.db.exec_(sql)
-                q.last()
-                if q.at()==0:
-                    sql = "update xpoitypes set poitype='%s' where enid=%s and lang='%s'" % (s, id, lang)
-                else:
-                    sql = "insert into xpoitypes (poitype, enid, lang) values('%s', %s, '%s')" % (s, id, lang)
+        idx_list = self.tableView_lists.selectionModel().selectedIndexes()
+        if len(idx_list) < 2: return
+        
+        # Identifier is in the second column (moa_code or vac_code)
+        id_val = str(self.model.data(idx_list[1]))
+        list_type = self.comboBox_lists.currentText()
+        
+        if list_type == 'Species':
+            sql = f"UPDATE xspecies SET name_en='{new_text}' WHERE moa_code='{id_val}'"
+        elif list_type == 'Diseases':
+            sql = f"UPDATE xdiseases SET name_en='{new_text}' WHERE moa_code='{id_val}'"
+        elif list_type == 'Vaccines':
+            sql = f"UPDATE xvaccines SET vac_name='{new_text}' WHERE vac_code='{id_val}'"
+        else:
+            return
 
-        q = self.db.exec_(sql)
+        self.db.exec_(sql)
         self.db.commit()
-        self.db.close()
         self.tabUpdate()
-        self.lineEdit_translation.setText('')
-        self.tableView_lists.setFocus()
-
+        self.lineEdit_translation.clear()
